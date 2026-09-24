1a4b041d6872ed7e34e3d0b85fcd745dc22985f4
francescodec88 <francescodec.88@gmail.com>
2026-09-24 09:00:09 +0200

    Deploy static site – move files to repository root

--- extract_zip.py
+++
@@ -1,6 +0,0 @@
-import zipfile, os, sys
-zip_path = 'CallCenterViolazioni_main.zip'
-extract_path = 'repo'
-with zipfile.ZipFile(zip_path, 'r') as z:
-    z.extractall(extract_path)
-print('Extraction completed')
